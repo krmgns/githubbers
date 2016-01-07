5dae7cc807375b7c9ee13659216db3b0d3046d54
Kerem Gunes <qeremy@gmail.com>
2016-01-07 22:24:21 +0200

    add "src" dir.

---
+++ src/gc_inc.php
@@ -0,0 +1,27 @@
+<?php
+// dump functions
+function pre($input, $exit = false){
+    if ($input === null) {
+        printf("NULL\n");
+    } else {
+        $input = print_r($input, true);
+        $input = preg_replace('~\[(\w+):.+(\\\\?.*?):private\]~sm', '[\\1:private]', $input);
+        printf("%s\n", $input);
+    }
+    if ($exit) {
+        exit;
+    }
+}
+function prd($input, $exit = false){
+    var_dump($input);
+    if ($exit) {
+        exit;
+    }
+}
+function prj($input, $exit = false) {
+    if (is_string($input)) {
+        $input = json_decode($input);
+    }
+    $input = json_encode($input, JSON_PRETTY_PRINT);
+    pre($input, $exit);
+}
