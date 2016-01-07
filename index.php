81db09361ea6d3167e048c38b7b7fab590429d86
Kerem Gunes <qeremy@gmail.com>
2016-01-07 23:12:20 +0200

    test GraphCommons-PHP.

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 header('content-type: text/plain; charset=utf-8');
 
+// hey!
+chdir(__dir__);
+
+// api cred.
+define('APIKEY', file_get_contents('./.apikey'));
+
 require('./src/gc_inc.php');
 require('./src/gc.php');
+
+use GraphCommons\GraphCommons;
+use GraphCommons\Graph\Graph;
+use GraphCommons\Graph\Signal;
+use GraphCommons\Graph\SignalCollection;
+
+prd(gc_ping());
