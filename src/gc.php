22656f59f7d6d8002279fc1de4623d465b774a76
Kerem Gunes <qeremy@gmail.com>
2016-01-07 23:12:06 +0200

    add GraphCommons-PHP.

--- src/gc.php
+++ src/gc.php
@@ -1,1 +1,23 @@
-<?php
+<?php defined('APIKEY') or die('...');
+
+$autoload = require('./src/graphcommons-php/GraphCommons/Autoload.php');
+$autoload->register();
+
+use GraphCommons\GraphCommons;
+
+function gc() {
+    if (!isset($GLOBALS['gc'])) {
+        $GLOBALS['gc'] = new GraphCommons(APIKEY);
+    }
+    return $GLOBALS['gc'];
+}
+
+function gc_ping() {
+    try {
+        $gc = gc();
+        $gc->api->status();
+        return $gc->client->response->ok();
+    } catch (\Exception $e) {
+        return false;
+    }
+}
