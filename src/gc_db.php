d99beb615aceb93a9837ed9f4550f0ee9bd51804
Kerem Gunes <qeremy@gmail.com>
2016-01-08 00:05:08 +0200

    add db user func's.

--- src/gc_db.php
+++ src/gc_db.php
@@ -1,6 +1,6 @@
 <?php
-function gc_db_repo($name) {
-    $file = sprintf('./db/repo/%s.json', $name);
+function gc_db_file($type, $name) {
+    $file = sprintf('./db/%s/%s.json', $type, $name);
     if (!is_file($file)) {
         touch($file);
     }
@@ -8,9 +8,17 @@ function gc_db_repo($name) {
 }
 
 function gc_db_get_repo($name) {
-    return json_decode(file_get_contents(gc_db_repo($name)), true);
+    return json_decode(file_get_contents(gc_db_file('repo', $name)), true);
 }
 
 function gc_db_put_repo($name, array $data) {
-    return file_put_contents(gc_db_repo($name), json_encode($data));
+    return (bool) file_put_contents(gc_db_file('repo', $name), json_encode($data));
+}
+
+function gc_db_get_user($name) {
+    return json_decode(file_get_contents(gc_db_file('user', $name)), true);
+}
+
+function gc_db_put_user($name, array $data) {
+    return (bool) file_put_contents(gc_db_file('user', $name), json_encode($data));
 }
